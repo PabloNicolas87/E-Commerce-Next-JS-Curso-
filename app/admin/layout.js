75745fae45dc60f8f8d390d0885efee2204b4c3a
Pablo Girone <gironepablo@gmail.com>
2024-09-16 18:12:53 -0300

    Pre-Entrega v3-17

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
 import { useAuthContext } from '../context/authContext';
-import LoginPage from '../login/PageLogin';
+import LoginPage from '../login/page';
 
 const AdminLayout = ({ children }) => {
     const { user } = useAuthContext();
